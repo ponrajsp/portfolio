2358c5a7bbf37cd7ac1bb36fcd6222dfe5710a1c
ponraj.subbaian@skybooking.info <ponraj.subbaian@skybooking.info>
2024-08-06 18:33:06 +0700

    footer styles design updated

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -28,7 +28,7 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
+        <Link href="https://ponraj-portfolio.vercel.app">
           <a className="underline underline-offset-1">Ponraj Subbaian</a>
         </Link>
       </h1>
